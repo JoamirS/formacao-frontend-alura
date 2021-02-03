f902b944359b58e1b7428aa9cd5c65a11c158504
Joamir Silveira <jmrsilveira@outlook.com>
2021-02-03 01:11:48 -0300

    Fazendo operações Aritiméticas

--- Chapter 04/Class_01.js
+++ Chapter 04/Class_01.js
@@ -1,1 +1,9 @@
-console.log("Meu primeiro programa");
+console.log("Meu primeiro programa: Trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade + 2);
+
+const idadeSomada = idade + 2;
+console.log("A idade somada é "+ idadeSomada + " anos")
