e34dabf672a45cb49c1f8ca37ab8f7a5e9340316
JoamirS <jmrsilveira@outlook.com>
2021-02-07 20:22:50 -0300

    Math operations

--- Chapter 04/Class_03.js
+++ Chapter 04/Class_03.js
@@ -1,3 +1,19 @@
 console.log("Operações Aritméticas")
 
 console.log(10 + 8 * 2);
+
+console.log((10 + 8) * 2);
+
+console.log("Ano " + 2021);
+
+/* O JavaScript não converte automaticamente um número para inteiro, quando é feita uma soma entre
+dois números. Já quando é feita outro tipo de operação aritmética, não precisamos converter a String
+utilizando a função Parse, por exemplo.
+*/
+
+console.log(parseInt("8") + parseInt("8"));
+console.log("10" / "2");
+
+// Quando eu realizo uma operação entre um número e uma String, ele retorna um NAN, isto é, Not a Number.
+
+console.log("Ricardo" / "2");
