e40565f37903db34f448eba9fe5ccd801502a971
JoamirS <jmrsilveira@outlook.com>
2021-02-07 22:55:34 -0300

    list

---
+++ Chapter 04/Class_05.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com listas");
+
+const salvador = "Salvador";
+const saoPaulo = "São Paulo";
+const rioDeJaneiro = "Rio";
+
+// Criando uma lista com os destinos possíveis, inserindo os valores das coonstantes nela.
+const listaDeDestinos = new Array(salvador, saoPaulo, rioDeJaneiro);
+console.log(listaDeDestinos);
+
+// Inserindo um dado na lista após ela ser declarada.
+listaDeDestinos.push("São Luís");
+console.log(listaDeDestinos);
