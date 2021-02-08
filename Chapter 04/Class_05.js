5eb07854537b266882500298cf1cb5b14eca8e2e
JoamirS <jmrsilveira@outlook.com>
2021-02-07 23:20:48 -0300

    list

--- Chapter 04/Class_05.js
+++ Chapter 04/Class_05.js
@@ -11,3 +11,13 @@ console.log(listaDeDestinos);
 // Inserindo um dado na lista após ela ser declarada.
 listaDeDestinos.push("São Luís");
 console.log(listaDeDestinos);
+
+/* Removendo um item da lista através do comando splice. 2 é a posição do item que desejamos
+deletar, e o 1 é a quantidade de items que queremos deletar a partir da posição escolhida.
+*/
+listaDeDestinos.splice(2, 1);
+console.log(listaDeDestinos);
+
+// Acessar um elemento específico da lista.
+
+console.log(listaDeDestinos[1]);
