dd723c019bc292f7f088dd5ab89f164db4679fc0
JoamirS <jmrsilveira@outlook.com>
2021-02-07 22:16:37 -0300

    Assignment of variables

---
+++ Chapter 04/Class_04.js
@@ -0,0 +1,16 @@
+console.log("Atribuição de variáveis");
+
+const idade = 29;
+let primeiroNome = "Joamir";
+const sobrenome = "Moraes";
+
+/*
+Posso concatenar duas Strings de duas maneiras: adicionando um espaço em branco entre duas strings ou
+usando a vírgula para separar dois as duas vaŕiaveis.
+*/
+// console.log(primeiroNome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
