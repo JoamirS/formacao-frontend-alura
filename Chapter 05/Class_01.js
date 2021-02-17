7a2bc795410637035fc2d9b6c0acfb28c084cae7
Joamir Silveira <jmrsilveira@outlook.com>
2021-02-17 06:32:03 -0300

    Update Class

--- Chapter 05/Class_01.js
+++ Chapter 05/Class_01.js
@@ -1,16 +1,30 @@
-class Client{
+class Client {
     name;
     CPF;
+}
+
+class Account {
     agency;
     balance;
+    
+    withdrawMoney(value){
+        if (this.balance = value){
+            this.balance -= value;
+        }
+    }
 }
 
 const clientOne = new Client();
 clientOne.name = "Ricardo";
-clientOne.CPF = '05605605600';
-clientOne.agency = 1001;
-clientOne.balance = 0;
+clientOne.CPF = 05605605600;
 
-const ClientTwo = new CCl
+const clientTwo = new Client();
+clientTwo.name = "Alice";
+clientTwo.CPF = 05205205200;
+
+const joamirAccount = new Account;
+joamirAccount.balance = 0;
+joamirAccount.agency = 1001;
 
 console.log(clientOne);
+console.log(clientTwo)
