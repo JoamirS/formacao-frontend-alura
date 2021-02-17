5e3798471d1a081ec5d285588c3b9c86ed3ceee6
Joamir Silveira <jmrsilveira@outlook.com>
2021-02-17 00:16:36 -0300

    Create Class_01

---
+++ Chapter 05/Class_01.js
@@ -0,0 +1,16 @@
+class Client{
+    name;
+    CPF;
+    agency;
+    balance;
+}
+
+const clientOne = new Client();
+clientOne.name = "Ricardo";
+clientOne.CPF = '05605605600';
+clientOne.agency = 1001;
+clientOne.balance = 0;
+
+const ClientTwo = new CCl
+
+console.log(clientOne);
