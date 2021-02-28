b224d4d16c7a750ac4f15069ebba01fec1530c2f
JoamirS <jmrsilveira@outlook.com>
2021-02-27 23:17:04 -0300

    Adding the constructor

--- Chapter 04/Class_08.js
+++ Chapter 04/Class_08.js
@@ -1,28 +1,39 @@
+
 class Cliente {
-    nome;
-    cpf;
+    constructor(nome, cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
 }
 
-class ContaCorrente {
-    agencia;
-    saldo;
+class ContaCorrente{
+    constructor(agencia, saldo){
+        this.agencia = agencia;
+        this.saldo = saldo;
+    }
 
     sacar(valor){
         if(this.saldo >= valor){
             this.saldo -= valor;
             console.log("Valor sacado com sucesso.\n O valor sacado foi: " + valor);
-            console.log("Seu saldo: " + this.saldo)
+            console.log("Seu saldo: " + this.saldo);
         } else {
-            console.log("Você não possui saldo suficiente para este saque.")
+            console.log("Você não possui saldo suficiente para este saque.");
+        }
+    }
+
+    depositar(valor){
+        if (valor > 0 ){
+            this.saldo += valor;
         }
     }
 }
 
-const cliente1 = new Cliente();
-cliente1.cpf = 00100100101;
-cliente1.nome = "Joamir";
+let cliente1 = new Cliente("Joamir", 00100100101);
 
 contaCorrenteJoamir = new ContaCorrente();
 contaCorrenteJoamir.saldo = 0;
 contaCorrenteJoamir.agencia = 1010;
+
 contaCorrenteJoamir.sacar(10);
+contaCorrenteJoamir.depositar(100);
