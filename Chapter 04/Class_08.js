f3723db18f51859195c10c43eadd6d8387b265d1
JoamirS <jmrsilveira@outlook.com>
2021-03-13 22:31:00 -0300

    adicionando o else em depositar

--- Chapter 04/Class_08.js
+++ Chapter 04/Class_08.js
@@ -9,14 +9,14 @@ class Cliente {
 class ContaCorrente{
     constructor(agencia, saldo){
         this.agencia = agencia;
-        this.saldo = saldo;
+        this.#saldo = saldo;
     }
 
     sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
             console.log("Valor sacado com sucesso.\n O valor sacado foi: " + valor);
-            console.log("Seu saldo: " + this.saldo);
+            console.log("Seu saldo: " + this.#saldo);
         } else {
             console.log("Você não possui saldo suficiente para este saque.");
         }
@@ -24,16 +24,21 @@ class ContaCorrente{
 
     depositar(valor){
         if (valor > 0 ){
-            this.saldo += valor;
+            this.#saldo += valor;
+        } else {
+            console.log("Valor insuficiente")
         }
+            
     }
 }
 
 let cliente1 = new Cliente("Joamir", 00100100101);
 
 contaCorrenteJoamir = new ContaCorrente();
-contaCorrenteJoamir.saldo = 0;
+contaCorrenteJoamir.saldo = 1000;
 contaCorrenteJoamir.agencia = 1010;
 
 contaCorrenteJoamir.sacar(10);
 contaCorrenteJoamir.depositar(100);
+console.log(contaCorrenteJoamir);
+
