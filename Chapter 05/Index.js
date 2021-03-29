aebf1ca818ff88f4df72993d955603b39838c755
JoamirS <jmrsilveira@outlook.com>
2021-03-28 22:44:39 -0300

    importing Class

--- Chapter 05/Index.js
+++ Chapter 05/Index.js
@@ -1,7 +1,17 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente"
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const clienteUm = new Cliente();
 clienteUm.nome = "Joamir";
-clienteUm.CPF = 002502502502;
+clienteUm.CPF = 29765158327;
 
+const contaDoJoamir = new ContaCorrente();
+contaDoJoamir.agencia = 1001;
+contaDoJoamir.saldo = 0;
+contaDoJoamir.depositar(500);
+
+console.log(clienteUm.nome);
+console.log(clienteUm.CPF);
+
+
+console.log("A conta do Joamir possui R$ " + contaDoJoamir.saldo + " de Saldo");
