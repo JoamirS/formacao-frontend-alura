fb9e78cfa2231ecf39d175d3a66c1fc4cc558ebf
JoamirS <jmrsilveira@outlook.com>
2021-04-02 18:58:52 -0300

    getters and setters

--- Chapter 05/ContaCorrente.js
+++ Chapter 05/ContaCorrente.js
@@ -1,11 +1,29 @@
+import { Cliente } from "./Cliente.js";
+
  export class ContaCorrente {
     agencia;
-    cliente;
-    saldo;
-    
+    _cliente;
+
+    set cliente(novoValor){
+        if (novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+            
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    _saldo = 0;
+
+    get saldo(){
+        return this._saldo;
+    }
+
     sacar(valor){
-        if (this.saldo >= valor){
-            this.saldo -= valor;
+        if (this._saldo >= valor){
+            this._saldo -= valor;
             return valor;
         }
     }
@@ -14,7 +32,7 @@
         if(valor <= 0){
             return;
         }
-        this.saldo += valor;
+        this._saldo += valor;
     }
 
     transferir(valorTransferencia, contaDestino){
--- Chapter 05/Index.js
+++ Chapter 05/Index.js
@@ -7,7 +7,7 @@ clienteUm.CPF = 29765158327;
 
 const contaDoJoamir = new ContaCorrente();
 contaDoJoamir.agencia = 1001;
-contaDoJoamir.saldo = 0;
+contaDoJoamir._saldo = 0;
 contaDoJoamir.cliente = clienteUm;
 
 const ClienteDois = new Cliente();
@@ -16,13 +16,14 @@ ClienteDois.CPF = 80166205311;
 
 const contaDoBruce = new ContaCorrente();
 contaDoBruce.agencia = 2002;
-contaDoBruce.saldo = 0;
+contaDoBruce._saldo = 0;
 contaDoBruce.cliente = ClienteDois;
 
 contaDoJoamir.depositar(100);
 contaDoJoamir.sacar(10);
 
-contaDoJoamir.transferir(10, contaDoBruce);
+let valorParaTrasferir = 10;
+contaDoJoamir.transferir(valorParaTrasferir, contaDoBruce);
 
 console.log(contaDoJoamir);
 console.log(contaDoBruce);
