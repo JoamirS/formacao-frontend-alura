1484776af8efbba639470394913ce86a0dcddd9f
JoamirS <jmrsilveira@outlook.com>
2021-04-04 19:39:35 -0300

    Classe mãe

--- Chapter 06/Conta.js
+++ Chapter 06/Conta.js
@@ -1,29 +1,42 @@
-export class Conta{
-    constructor(saldoInicial, cliente, agencia){
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
         this._saldo = saldoInicial;
         this._cliente = cliente;
-        this.agencia = agencia;
+        this._agencia = agencia;
     }
 
-    sacar(valor){
-        if(this._saldo >= valor){
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
             this._saldo -= valor;
             return valor;
         }
     }
 
-    depositar(valor){
-        if(valor <= 0)
-        {
+    depositar(valor) {
+        if (valor <= 0) {
             return;
-        } 
-        this._saldo += valor;           
+        }
+        this._saldo += valor;
     }
 
-    tranferir(valor, conta){
-        
+    tranferir(valor, conta) {
+
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        
+
     }
 }
