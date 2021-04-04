1536dd41e188034567384f5bf8c72bbca0924b8b
JoamirS <jmrsilveira@outlook.com>
2021-04-04 19:41:27 -0300

    index chamando as classes

--- Chapter 06/index.js
+++ Chapter 06/index.js
@@ -1,16 +1,15 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 import {ContaPoupanca} from "./ContaPoupanca.js";
-import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Joamir", 11122233309);
 const cliente2 = new Cliente("Harvey", 88822233309);
 
-const contaCorrenteJoamir = new Conta(0, cliente1, 1001);
+const contaCorrenteJoamir = new ContaCorrente(0, cliente1, 1001);
 contaCorrenteJoamir.depositar(500);
 contaCorrenteJoamir.sacar(100);
 
-const contaPoupancaHarvey = new Conta(50, cliente2, 1002);
+const contaPoupancaHarvey = new ContaPoupanca(50, cliente2, 1002);
 
 console.log(contaCorrenteJoamir);
 console.log(contaPoupancaHarvey);
