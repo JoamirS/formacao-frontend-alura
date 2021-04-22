a2efb69a5729db4c77f6093297bdc91f6fa4983e
JoamirS <jmrsilveira@outlook.com>
2021-04-21 23:20:13 -0300

    accessing class and id

--- Chapter 07/introducao-javascript/js/principal.js
+++ Chapter 07/introducao-javascript/js/principal.js
@@ -3,3 +3,13 @@ console.log(titulo);
 console.log(titulo.textContent);
 
 titulo.textContent = "Nutricionista Aparecida";
+
+var paciente = window.document.getElementsByClassName("primeiro-paciente");
+var tdPeso = window.document.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+
