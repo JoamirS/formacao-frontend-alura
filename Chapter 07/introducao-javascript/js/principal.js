3edcc9c143aad0265c2983460465c1e9793665cc
JoamirS <jmrsilveira@outlook.com>
2021-04-25 12:37:55 -0300

    adicionando validacoes

--- Chapter 07/introducao-javascript/js/principal.js
+++ Chapter 07/introducao-javascript/js/principal.js
@@ -6,10 +6,29 @@ titulo.textContent = "Nutricionista Aparecida";
 
 var paciente = window.document.getElementsByClassName("primeiro-paciente");
 var tdPeso = window.document.querySelector(".info-peso");
-
 var peso = tdPeso.textContent;
+var tdAltura = window.document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
-console.log(paciente);
-console.log(tdPeso);
-console.log(peso);
+var tdImc = window.document.querySelector(".info-imc");
+
+var pesoEValido = true;
+var alturaEvalida = true;
+
+if (peso < 0 || peso > 1000){
+    console.log("Peso inválido");
+    pesoEValido = false;
+    tdImc.textContent = "Peso inválido"
+}
+
+if (altura < 0 || altura > 3.00){
+    console.log("Altura inválida");
+    alturaEvalida = false;
+    tdImc.textContent = "Altura Inválida"
+}
+
+if (alturaEvalida && pesoEValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
 
