1c8db6c3cc9927702b977c1c4050272ae02a9c4c
JoamirS <jmrsilveira@outlook.com>
2021-06-21 22:45:21 -0300

    adicionando o javascript form

--- Chapter 07/introducao-javascript/js/calcula-imc.js
+++ Chapter 07/introducao-javascript/js/calcula-imc.js
@@ -36,39 +36,3 @@ for (var i = 0; i < pacientesLista.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
-
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-
-botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault();
-
-    var form = document.querySelector("#formulario-adiciona");
-
-    alturaForm = form.altura.value;
-    pesoForm = form.peso.value;
-    gorduraForm = form.gordura.value;
-    nomeForm = form.nome.value;
-
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nomeForm;
-    pesoTd.textContent = pesoForm;
-    alturaTd.textContent = alturaForm;
-    gorduraTd.textContent = gorduraForm;     
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes")
-
-    tabela.appendChild(pacienteTr);
-}
-)
---
+++ Chapter 07/introducao-javascript/js/form.js
@@ -0,0 +1,35 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#formulario-adiciona");
+
+    alturaForm = form.altura.value;
+    pesoForm = form.peso.value;
+    gorduraForm = form.gordura.value;
+    nomeForm = form.nome.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nomeForm;
+    pesoTd.textContent = pesoForm;
+    alturaTd.textContent = alturaForm;
+    gorduraTd.textContent = gorduraForm;     
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+}
+)
