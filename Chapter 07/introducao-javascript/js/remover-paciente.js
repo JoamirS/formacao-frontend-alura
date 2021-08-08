1468e201a5f71c6764236ad942ddb7c47459378a
JoamirS <jmrsilveira@outlook.com>
2021-08-08 17:56:21 -0300

    removendo um item da tabela

--- Chapter 07/introducao-javascript/js/remover-paciente.js
+++ Chapter 07/introducao-javascript/js/remover-paciente.js
@@ -1,7 +1,7 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("#tabela-pacientes");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-    this.remove();
-    });
+tabela.addEventListener("dblclick", function (event) {
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode;
+    paiDoAlvo.remove();
 });
